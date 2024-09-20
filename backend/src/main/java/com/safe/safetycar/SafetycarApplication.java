246774c55ed346407fe548ae44bb7c9fba915202
임권 <limkwon18@gmail.com>
2024-09-20 10:26:11 +0900

    init : backend

---
+++ backend/src/main/java/com/safe/safetycar/SafetycarApplication.java
@@ -0,0 +1,13 @@
+package com.safe.safetycar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetycarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafetycarApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/safe/safetycar/SafetycarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safe.safetycar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetycarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
