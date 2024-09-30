f0e61608f05c3c94fc01b78e2d657513d55dfe5a
임권 <limkwon18@gmail.com>
2024-09-30 23:25:19 +0900

    add : debug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -71,6 +71,7 @@ function ShowCCTV2() {
   ws.onmessage = function(msg) {
     let newArr = [...frameSrcArr];
     const idx = msg.data.slice(0,1);
+    console.log(msg.data);
     console.log(idx);
     console.log(msg.data);
     newArr[0] = URL.createObjectURL(msg.data).slice(1);
