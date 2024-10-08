2bd22bdbc8d0cebe97cff4c14227cc058a9250fa
서근범 <seo6893@naver.com>
2024-10-08 14:09:50 +0900

    fix: remove build file

--- ROS2/build/my_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == 'c:\\python37':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = 'C:\\Users\\SSAFY\\Desktop\\Second_Project\\Project\\S11P21B209\\ROS2\\install\\my_package'
--- ROS2/build/ssafy_msgs/ssafy_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/SSAFY/Desktop/Second_Project/Project/S11P21B209/ROS2/build/ssafy_msgs/ssafy_msgs__py
-# Build directory: C:/Users/SSAFY/Desktop/Second_Project/Project/S11P21B209/ROS2/build/ssafy_msgs/ssafy_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
