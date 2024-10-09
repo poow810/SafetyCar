28e2f9390e51a0da5b86564a6c5f1775a5c93162
poow810 <poow810@naver.com>
2024-10-09 10:33:08 +0900

    [AI] fix : host 설정 변경

--- coordinate/Python_Server.py
+++ coordinate/Python_Server.py
@@ -730,4 +730,4 @@ async def transform_point(
 # 서버 실행
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
