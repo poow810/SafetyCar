ad2e04899bee3f6ca4256404b02d6e9a1616fa14
poow810 <poow810@naver.com>
2024-10-09 17:51:39 +0900

    [FE] feat : 강제 복귀 명령 axios 요청 추가

--- frontend/src/components/navibar.js
+++ frontend/src/components/navibar.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom'; // useNavigate 훅을 import
 import '../styles/navibar.css'; // CSS 파일 import
+import axios from 'axios';
 
 const Navbar = () => {
-  const navigate = useNavigate(); // navigate 함수 생성
+  const navigate = useNavigate(); 
+  const PYTHON_URL = process.env.REACT_APP_PYTHON_URL; 
+
+  const handleRotateClick = async () => {
+    try {
+      const response = await axios.get(`${PYTHON_URL}/safety_Car/halt`);
+      console.log('Response:', response.data);
+    } catch (error) {
+      console.error('Error making GET request:', error);
+      alert('요청 처리 중 오류가 발생했습니다.');
+    }
+  };
 
   return (
     <div className="navbar">
@@ -13,10 +25,10 @@ const Navbar = () => {
       <div className="nav-item" onClick={() => navigate('/step1')}>
         <img src="/assets/navi/Settings.png" alt="Settings" className="nav-icon" /> {/* Settings 아이콘 */}
       </div>
-      <div className="nav-item" onClick={() => navigate('/image')}>
+      <div className="nav-item">
         <img src="/assets/navi/Mail.png" alt="Mail" className="nav-icon" /> {/* Mail 아이콘 */}
       </div>
-      <div className="nav-item" onClick={() => navigate('/rotate')}>
+      <div className="nav-item" onClick={handleRotateClick}>
         <img src="/assets/navi/Rotate cw (1).png" alt="Rotate" className="nav-icon" /> {/* Rotate 아이콘 */}
       </div>
     </div>
